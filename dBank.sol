f4a7a260a4a0e88475febd87c5e488e26fb18a79
Joel John <94542052+joeljohn27@users.noreply.github.com>
2021-12-12 15:14:39 +0530

    Update dBank.sol

--- dBank.sol
+++ dBank.sol
@@ -7,13 +7,20 @@ contract dBank {
 
   //assign Token contract to variable
   Token private token;
+  
   //add mappings
-  mapping(address => uint) public etherBalanceOf;
   mapping(address => uint) public depositStart;
+  mapping(address => uint) public etherBalanceOf;
+  mapping(address => uint) public collateralEther;
+   
   mapping(address => bool) public isDeposited;
+  mapping(address => bool) public isBorrowed;
+  
   //add events
   event Deposit(address indexed user,uint etherAmount,uint timeStart);
   event Withdraw(address indexed user,uint etherAmount,uint depositTime,uint interest);
+  event Borrow(address indexed user, uint collateralEtherAmount, uint borrowedTokenAmount);
+  event PayOff(address indexed user, uint fee);
 
   //pass as constructor argument deployed Token contract
   constructor(Token _token) {
@@ -22,6 +29,7 @@ contract dBank {
   }
 
   function deposit() payable public {
+  
     //check if msg.sender didn't already deposited funds
     require(isDeposited[msg.sender] == false,"Error,deposit already active");
     //check if msg.value is >= than 0.01 ETH
@@ -40,6 +48,7 @@ contract dBank {
   }
 
   function withdraw() payable public {
+  
     //check if msg.sender deposit status is true
     require(isDeposited[msg.sender] == true,"Error,no previous deposit");
     uint userBalance = etherBalanceOf[msg.sender];
@@ -68,30 +77,48 @@ contract dBank {
   }
 
   function borrow() payable public {
+  
     //check if collateral is >= than 0.01 ETH
     //check if user doesn't have active loan
 
     //add msg.value to ether collateral
 
+    require(msg.value>=1e16, 'Error, collateral must be >= 0.01 ETH');
+    require(isBorrowed[msg.sender] == false, 'Error, loan already taken');
+
+    //this Ether will be locked till user payOff the loan
+    collateralEther[msg.sender] = collateralEther[msg.sender] + msg.value;
+
     //calc tokens amount to mint, 50% of msg.value
+    uint tokensToMint = collateralEther[msg.sender] / 2;
 
     //mint&send tokens to user
+    token.mint(msg.sender, tokensToMint);
 
     //activate borrower's loan status
-
+    isBorrowed[msg.sender] = true;
+    
     //emit event
+    emit Borrow(msg.sender, collateralEther[msg.sender], tokensToMint);
   }
 
   function payOff() public {
+  
     //check if loan is active
+    require(isBorrowed[msg.sender] == true, 'Error, loan not active');
     //transfer tokens from user back to the contract
+    require(token.transferFrom(msg.sender, address(this), collateralEther[msg.sender]/2), "Error, can't receive tokens"); //must approve dBank 1st
 
-    //calc fee
+    uint fee = collateralEther[msg.sender]/10; //calc 10% fee
 
     //send user's collateral minus fee
+    msg.sender.transfer(collateralEther[msg.sender]-fee);
 
     //reset borrower's data
-
-    //emit event
+    collateralEther[msg.sender] = 0;
+    isBorrowed[msg.sender] = false;
+    
+     //emit event
+    emit PayOff(msg.sender, fee);
   }
 }
